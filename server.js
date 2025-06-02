68aef0fd725a9e7dc9c46579cd8d8b6b51882cfa
ManarSwar97 <manarswar97@gmail.com>
2025-06-02 11:30:08 +0300

    backend file initialization

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const logger = require('morgan')
+
+const PORT = process.env.PORT || 3001
+
+const db = require('./db')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
