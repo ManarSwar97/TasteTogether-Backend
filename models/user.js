bc605d5231c5791914b711acd551588041fc047d
ManarSwar97 <manarswar97@gmail.com>
2025-06-03 11:26:58 +0300

    modify

--- models/user.js
+++ models/user.js
@@ -1,1 +1,19 @@
+const { Schema } = require('mongoose')
+
+const userSchema = new Schema(
+  {
     first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    image: { type: String },
+    type_of_food:{
+        type:[String], 
+        required: true
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = userSchema
